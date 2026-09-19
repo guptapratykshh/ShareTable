300824b7c175af534b514b9ebf7bb6888efd860e
the-ivii <divyanshitalreja12@gmail.com>
2026-09-19 11:40:05 +0530

    Remove the unused Esri attribution helper
    Signed-off-by: the-ivii <divyanshitalreja12@gmail.com>

--- client/src/utils/mapTiles.ts
+++ client/src/utils/mapTiles.ts
@@ -2,7 +2,3 @@
 export function mapTileUrl(_dark = false) {
   return "https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}";
 }
-
-export function mapTileAttribution(_dark = false) {
-  return "Tiles &copy; Esri &mdash; Source: Esri, OpenStreetMap";
-}
