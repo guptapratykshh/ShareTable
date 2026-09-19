273ce13f52e1be5863c71a192cdbbe46760c1ffc
the-ivii <divyanshitalreja12@gmail.com>
2026-09-19 11:40:05 +0530

    Drop Esri credit text from map tiles
    Signed-off-by: the-ivii <divyanshitalreja12@gmail.com>

--- client/src/components/ThemedTileLayer.tsx
+++ client/src/components/ThemedTileLayer.tsx
@@ -1,6 +1,6 @@
 import { TileLayer } from "react-leaflet";
 import { useTheme } from "../context/ThemeContext";
-import { mapTileAttribution, mapTileUrl } from "../utils/mapTiles";
+import { mapTileUrl } from "../utils/mapTiles";
 
 export function ThemedTileLayer() {
   const { theme } = useTheme();
@@ -8,7 +8,7 @@ export function ThemedTileLayer() {
   return (
     <TileLayer
       key="esri-street"
-      attribution={mapTileAttribution(dark)}
+      attribution=""
       url={mapTileUrl(dark)}
       maxNativeZoom={17}
       maxZoom={17}
